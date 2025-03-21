9a31998e556c809f010cd33efa763994ac5983c0
dazzle50 <dazzle50@gmail.com>
2025-03-21 18:36:14 +0000

    Missed Predecessors + TaskResources + TimeSpan in previous commit

---
+++ src/rjc/jplanner/plan/TimeSpan.java
@@ -0,0 +1,42 @@
+/**************************************************************************
+ *  Copyright (C) 2025 by Richard Crook                                   *
+ *  https://github.com/dazzle50/JPlannerFX                                *
+ *                                                                        *
+ *  This program is free software: you can redistribute it and/or modify  *
+ *  it under the terms of the GNU General Public License as published by  *
+ *  the Free Software Foundation, either version 3 of the License, or     *
+ *  (at your option) any later version.                                   *
+ *                                                                        *
+ *  This program is distributed in the hope that it will be useful,       *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *  GNU General Public License for more details.                          *
+ *                                                                        *
+ *  You should have received a copy of the GNU General Public License     *
+ *  along with this program.  If not, see http://www.gnu.org/licenses/    *
+ **************************************************************************/
+
+package rjc.jplanner.plan;
+
+/*************************************************************************************************/
+/********************************** Quantity of time with units **********************************/
+/*************************************************************************************************/
+
+public class TimeSpan
+{
+  private double m_num;
+  private char   m_units;
+
+  /**************************************** constructor ******************************************/
+  public TimeSpan()
+  {
+    // construct default time-span
+    m_num = 0.0;
+    m_units = 'd';
+  }
+
+  /**************************************** constructor ******************************************/
+  public TimeSpan( String str )
+  {
+  }
+}
